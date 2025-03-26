42d2fb82f8dd86b9c9985eac02656b9a84e9c708
rajpurohitmohit <rajpurohitmohit954@gmail.com>
2025-03-26 13:04:09 +0530

    Adding referance and pointer

--- swappingTwoNumbers.cpp
+++ swappingTwoNumbers.cpp
@@ -17,18 +17,28 @@ int swap_Reference(int &a, int &b){
 
 int swap_pointer(int *a, int *b){
     int temp;
-    temp = a;
-    a = b;
-    b = 
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 int main() {
     int a = 10;
     int b = 100;
     
-    swap(a, b);
+    swap_Value(a, b);
     cout << "CALL BY VALUE - " << endl;
     cout << "A: " << a << endl;
     cout << "B: " << b << endl;
     
+    swap_Reference(a, b);
+    cout << "CALL BY REFERENCE - " << endl;
+    cout << "A: " << a << endl;
+    cout << "B: " << b << endl;
+
+    swap_pointer(&a, &b);
+    cout << "CALL BY POINTER - " << endl;
+    cout << "A: " << a << endl;
+    cout << "B: " << b << endl;
+
     return 0;
 }
